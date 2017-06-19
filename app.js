37395d7475a3c76d304fd2806c73438c71fbb8e0
xcxerxes <xiac@jieshuibao.com>
2017-06-19 22:48:55 +0800

    支持跨域

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname,'static')))
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, sweet-token');
   next();
 });
 
